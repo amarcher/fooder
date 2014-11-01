0a4cce6042d50af9eb3e5b0fdb9c6dbd0cf8fb9b
Andrew Archer <archer@archer-macbookpro3.roam.corp.google.com>
2014-11-01 16:23:37 -0700

    select item

--- app/assets/javascripts/listView.js
+++ app/assets/javascripts/listView.js
@@ -9,6 +9,12 @@ ListView.prototype = {
 
   appendToList: function(item_partial){
     $(this.list).append(item_partial)
+    $(this.list + ' > li').last().hide()
+    $(this.list + ' > li').last().fadeIn(400)
+  }
+
+  selectItem: function(index){
+    $(this.list + ' > li:nth-of-type(' + index + ')').css("background-color",'rgb(228, 247, 219)'));
   }
 
 }
